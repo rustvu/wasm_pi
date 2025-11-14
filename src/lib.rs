d203ccb702eaa1afca847c8cb4fe8e77f7309f19
Peter Volgyesi <peter.volgyesi@gmail.com>
2025-11-14 07:19:10 -0600

    fixing deprecated warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
-use rand::random;
 use wasm_bindgen::prelude::*;
-
 
 // Note: this is not the most efficient rendering approach of
 // the results, but simple enough to understand.
@@ -9,22 +7,22 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn monte_carlo_pi(n_samples: u32) -> f64 {
-    let context = get_canvas_context();    
+    let context = get_canvas_context();
     let mut n_inside = 0;
     for _ in 0..n_samples {
         // Generate a random point in the unit square
-        let x = 2.0 * random::<f64>() - 1.0;
-        let y = 2.0 * random::<f64>() - 1.0;
+        let x = 2.0 * fastrand::f64() - 1.0;
+        let y = 2.0 * fastrand::f64() - 1.0;
         if x * x + y * y < 1.0 {
             n_inside += 1;
-            context.set_fill_style(&JsValue::from_str("red"));
+            context.set_fill_style_str("red");
         } else {
-            context.set_fill_style(&JsValue::from_str("blue"));
+            context.set_fill_style_str("blue");
         }
         // Draw a "point"
         context.fill_rect(x, y, 0.01, 0.01);
     }
-    
+
     4.0 * (n_inside as f64) / (n_samples as f64)
 }
 
@@ -52,7 +50,7 @@ fn get_canvas_context() -> web_sys::CanvasRenderingContext2d {
     context.clear_rect(0.0, 0.0, sx, sy);
 
     context.translate(sx / 2.0, sy / 2.0).unwrap();
-    context.scale(sx / 2.0, -sy / 2.0 ).unwrap();
+    context.scale(sx / 2.0, -sy / 2.0).unwrap();
 
     context
 }
